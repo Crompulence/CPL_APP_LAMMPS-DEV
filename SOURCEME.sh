94832612da421e18b905828791d602c9a0d4201d
Eduardo Ramos <er1414@imperial.ac.uk>
2017-06-12 17:32:33 +0100

    README.md added, examples folder created and deleted legacy cpl-socket.

--- SOURCEME.sh
+++ SOURCEME.sh
@@ -1,3 +1,9 @@
 export PATH=$(pwd)/bin:$PATH
-export LAMMPS_PATH=$(cat CODE_INST_DIR)
+
+if [ -f CODE_INST_DIR ]; then
+	export LAMMPS_PATH=$(cat CODE_INST_DIR)
+else
+	printf "ERROR:\n\tFile CODE_INST_DIR not found. Please provide one as described in the README.md file.\n"
+	return 1
+fi
 
