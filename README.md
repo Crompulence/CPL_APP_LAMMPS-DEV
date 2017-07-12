5073deb2e1e73a294934f38b012936ccef256140
Eduardo Ramos <er1414@imperial.ac.uk>
2017-07-12 18:51:59 +0100

    Added support for python LAMMPS package

---
+++ README.md
@@ -0,0 +1,3 @@
+
+NOTE: Compilation with the python LAMMPS package does not work if using MPICH different from the
+	  system one.
--- config/enable-packages.sh
+++ config/enable-packages.sh
@@ -2,7 +2,7 @@ MAKE=$1
 APP_DIR=$(pwd)
 LAMMPS_SRC_DIR=$(cat CODE_INST_DIR)/src
 cd $LAMMPS_SRC_DIR
+$MAKE "no-all"
 while read p; do
-  $MAKE "no-$p"
   $MAKE "yes-$p"
 done <$APP_DIR/config/lammps_packages.in
