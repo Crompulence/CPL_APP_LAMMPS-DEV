bb96b0c936089221337bd9861f0201ae1a00752e
edwardsmith999 <edward.smith05@imperial.ac.uk>
2018-01-31 18:30:15 +0000

    Some speculative code to loop over rigid bodies, not needed and not possible due to encapsulation. Can get all molecules in domain anyway.

--- src/USER-CPL/fix_cpl_force.h
+++ src/USER-CPL/fix_cpl_force.h
@@ -10,6 +10,8 @@ FixStyle(cpl/force, FixCPLForce)
 #include<memory>
 
 #include "fix.h"
+#include "fix_rigid.h"
+
 #include "cpl/cpl.h"
 #include "cpl/CPL_ndArray.h"
 #include "cpl/CPL_force.h"
@@ -31,8 +33,62 @@ private:
 
 	CPL::ndArray<double>* cfdBuf;
     std::vector<int> procPortion;
+    std::vector<double> fi;
+    //std::unique_ptr<LAMMPS_NS::FixRigid> clumpfix;
 
 };
 
 #endif
 #endif
+
+
+
+
+
+
+
+
+
+// ****************************************************************
+// Copied here for record of what not to do! Rigid mols are local!
+
+
+
+
+    //If we have rigid bodies, need to include these too
+    //int ifix = lmp->modify->find_fix("clumps");
+    //if (ifix < 0) lmp->error->all(FLERR,"Fix ID for fix clumps does not exist");
+
+    //auto clumpfix = dynamic_cast<LAMMPS_NS::FixRigid>(lmp->modify->fix[ifix]);
+    //int nbody = lmp->modify->fix[ifix]->nbody;
+    //int *nrigid = lmp->modify->fix[ifix]->nrigid; //Number of molecules in rigid body
+    //int *body2mol = lmp->modify->fix[ifix]->body2mol;
+
+        //LOOP over all rigid bodies and get forces
+//        // This would not work as these are protected
+//        for (int b = 0; b < nbody; ++b)
+//        {
+//            //Get number of molecules in rigid body
+//        	for (int bi = 0; bi < nrigid[b]; ++bi)
+//        	{
+
+//                //Get global molecular id from body id
+//                int i = body2mol[bi];
+//           		if (mask[i] & groupbit)
+//            	{
+
+//		            //Get local molecule data
+//		            mi = rmass[i];
+//		            radi = radius[i];
+//		            for (int n=0; n<3; n++){
+//		                xi[n]=x[i][n]; 
+//		                vi[n]=v[i][n]; 
+//		                ai[n]=f[i][n];
+//		            }
+
+//		            // Sum all the weights for each cell.
+//		            fxyz->pre_force(xi, vi, ai, mi, radi, pot);
+
+//            	}
+//            }
+//        }
