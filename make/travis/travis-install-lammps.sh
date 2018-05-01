fc448b938cd6593445229750492224988005d6b9
edwardsmith999 <edward.smith05@imperial.ac.uk>
2018-05-01 12:12:44 +0100

    Fixed lammps build script

--- make/travis/travis-install-lammps.sh
+++ make/travis/travis-install-lammps.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 set -e
-if [ -f "gitlammps" ]; then
+if [ -d "gitlammps" ]; then
     echo "Using cached LAMMPS"
 else
     case $1 in
-        lammps) set -x;
-            #Get LAMMPS
-            git clone https://github.com/lammps/lammps.git gitlammps
-        granlammps) set -x;
-            #Get GranLAMMPS first as this requires password
-            git clone https://edwardsmith999@bitbucket.org/granlammps/gitlammps.git gitlammps
-            cd ./gitlammps
-            git checkout common
-            cd ../
+      lammps) set -x;
+        #Get LAMMPS
+        git clone https://github.com/lammps/lammps.git gitlammps;;
+      granlammps) set -x;
+        #Get GranLAMMPS first as this requires password
+        git clone https://edwardsmith999@bitbucket.org/granlammps/gitlammps.git gitlammps
+        cd ./gitlammps
+        git checkout common
+        cd ../;;
+      *)
+        echo "Unknown lammps:" $1; exit 1;;
     esac
 fi
 
