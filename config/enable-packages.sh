6bfcfab3051eb80ded5a4200235fd456e126edcd
edwardsmith999 <edward.smith05@imperial.ac.uk>
2018-05-02 19:26:33 +0100

    Adjusted enable packages to work

--- config/enable-packages.sh
+++ config/enable-packages.sh
@@ -1,8 +1,10 @@
 MAKE=$1
 APP_DIR=$(pwd)
-LAMMPS_SRC_DIR=$(cat CODE_INST_DIR)/src
+LAMMPS_SRC_DIR=$(cat ../CODE_INST_DIR)/src
 cd $LAMMPS_SRC_DIR
 $MAKE "no-all"
+$MAKE ps
 while read p; do
   $MAKE "yes-$p"
-done <$APP_DIR/config/lammps_packages.in
+done <$APP_DIR/lammps_packages.in
+$MAKE ps
