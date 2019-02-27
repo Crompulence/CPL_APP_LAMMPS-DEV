1f44faa9e0c661d578ba5c11dd8c4253dcdbd6a7
Ed <es205@imperial.ac.uk>
2019-02-27 12:25:06 +0000

    Improved plotting flags and removed matplotlib dependency

--- test/Couette_coupled/Partial_overlap/check_LAMMPS_vs_analytical.py
+++ test/Couette_coupled/Partial_overlap/check_LAMMPS_vs_analytical.py
@@ -43,18 +43,21 @@ def check_LAMMPS_vs_Analytical(fdir, uwall=1., plotevolve=False):
     CAObj = CA(Re=Re, U=uwall, Lmin=liquidstart, Lmax=liquidend, 
                npoints=10*liquidbins, nmodes=100*liquidbins)
 
-    if "dynamic" in plotevolve:
+    if "dynamic" is plotevolve or plotevolve == True:
+        plotevolve = "dynamic" 
         import matplotlib.pyplot as plt
         fig, ax = plt.subplots(1,1)
         plt.ion()
         plt.show()
         recds = range(nsteps)
-    elif "summary" in plotevolve:
+    elif "summary" is plotevolve:
         import matplotlib.pyplot as plt
         fig, ax = plt.subplots(1,1)
         recds = [1, 3, 7, 24]
+    elif plotevolve == False or plotevolve == None:
+        plotevolve = "False"
     else:
-        raise IOError("plotevolve="+plotevolve+ 
+        raise IOError("plotevolve="+str(plotevolve)+ 
                       " which is unknown, should be dynamic or summary")
 
     ft = True
@@ -67,7 +70,7 @@ def check_LAMMPS_vs_Analytical(fdir, uwall=1., plotevolve=False):
             print("Last value read, exiting loop")
             break
 
-        if plotevolve:
+        if plotevolve != "False":
             # 0=Chunk 1=Coord1 2=Ncount 3=density/mass
             # 4=vx 5=vy 6=vz 7=temperature
             if rec in recds:
@@ -81,20 +84,20 @@ def check_LAMMPS_vs_Analytical(fdir, uwall=1., plotevolve=False):
         for i in range(mn, u.shape[0]+mx):
             indxs.append(find_nearest_indx(y_anal, u[i,1]*Ly))
 
-        if plotevolve:
+        if plotevolve != "False":
             if rec in recds:
                 ax.plot(u_anal[indxs], y_anal[indxs], 'k-x', label="vx Analytical")
 
         error = (u_anal[indxs] - u[mn:mx,4])/uwall
 
-        if "dynamic" in plotevolve:
+        if "dynamic" is plotevolve:
             ax.plot(error, y_anal[indxs], 'r-', label="Error")
         
         assert np.max(np.abs(error[:-1])) < 0.2
         timeerror.append(error)
         #print("Error = ",  np.sum(error))
 
-        if "dynamic" in plotevolve:
+        if "dynamic" is plotevolve:
 
             if ft:
                 plt.legend()
@@ -108,15 +111,15 @@ def check_LAMMPS_vs_Analytical(fdir, uwall=1., plotevolve=False):
 
     timeerror = np.array(timeerror)
 
-    if "dynamic" in plotevolve:
+    if "dynamic" is plotevolve:
         plt.ioff()
         plt.pcolormesh(timeerror[:,:-1].T, cmap=plt.cm.RdYlBu_r)
         plt.colorbar()
         plt.show()
-    else:
+    elif "summary" is plotevolve:
         ax.set_xlabel("$u$")
         ax.set_ylabel("$y$")
-        plt.savefig("LAMMPS_Validation.png", bbox_inches="tight")
+        plt.savefig("LAMMPS_Validation_uwall" + str(uwall) + ".png", bbox_inches="tight")
 
     #Check average error less than 4%
     #print(np.mean(np.abs(timeerror),0))
@@ -126,5 +129,5 @@ def check_LAMMPS_vs_Analytical(fdir, uwall=1., plotevolve=False):
 if __name__ == "__main__":
     for uwall in [0.6,0.7,0.8,0.9,1.0]:
         fdir = "./run" + str(uwall)
-        check_LAMMPS_vs_Analytical(fdir, uwall=uwall, plotevolve="dynamic")
+        check_LAMMPS_vs_Analytical(fdir, uwall=uwall, plotevolve="summary")
 
--- test/Couette_coupled/Partial_overlap/python_dummy/CFD_test_vs_Couette.py
+++ test/Couette_coupled/Partial_overlap/python_dummy/CFD_test_vs_Couette.py
@@ -44,13 +44,10 @@
 
 import numpy as np
 from mpi4py import MPI
-import matplotlib.pyplot as plt
 
 from CouetteAnalytical import CouetteAnalytical as CA
 
 from cplpy import CPL
-
-plotstuff=False
 
 #initialise MPI and CPL
 comm = MPI.COMM_WORLD
@@ -59,36 +56,18 @@ MD_COMM = CPL.init(CPL.CFD_REALM)
 
 ## Parameters of the cpu topology (cartesian grid)
 npxyz = [1, 1, 1]
+
 # FCC lattice, plus wall width plus buffer region
-wallwidth = 1.
-bufsize = 2.
 xyzL = [16.795961913825074, 45.349097, 16.795961913825074]
 xyz_orig = [0.0, 0.0, 0.0]
 ncxyz = [8, 8, 8]
 
-dy = xyzL[1]/float(ncxyz[1])
-
-Uwall = 1.
-dt = 0.005*50
-nu = 1.7
-liquidstart = xyz_orig[1]+2.*wallwidth+2.*bufsize
-liquidend = xyzL[1]-2.*wallwidth-2.*bufsize
-liquidregion = liquidend-liquidstart
-liquidbins = int(liquidregion/dy)
-Re = liquidregion/nu   #Note Reynolds in independent of velocity in analytical fn
-y = np.linspace(xyz_orig[1],  xyzL[1], ncxyz[1])
-
-CAObj = CA(Re=Re, U=Uwall, Lmin=liquidstart, Lmax=liquidend, npoints=liquidbins, nmodes=100*liquidbins)
+Uwall = 1.0
 
 #Setup coupled simulation
 cart_comm = MD_COMM.Create_cart([npxyz[0], npxyz[1], npxyz[2]])
 CPL.setup_cfd(cart_comm, xyzL, xyz_orig, ncxyz)
 recv_array, send_array = CPL.get_arrays(recv_size=4, send_size=3)
-
-if plotstuff:
-    fig, ax = plt.subplots(1,1)
-    plt.ion()
-    plt.show()
 
 for time in range(500):
 
@@ -101,17 +80,7 @@ for time in range(500):
         
     # recv data and plot
     recv_array, ierr = CPL.recv(recv_array)
-
-    #y_anal, u_anal = CAObj.get_vprofile(10*(time+1)*10)
-
     UBC = np.mean(recv_array,(1,2,3))
-    #ax.imshow(recv_array[3,:,0,:])
-    #ax.plot(y, np.mean(recv_array[3,:,:,:], (0,2))/1e2, '-or')
-    #ax.plot(y, np.mean(recv_array[0,:,:,:], (0,2)), '-ob')
-    #ax.plot(y_anal, u_anal, '-k')
-    #ax.set_ylim([-0.1,1.4])
-    #plt.pause(0.002)
-    #plt.cla()
 
 comm.Barrier()
 CPL.finalize()
