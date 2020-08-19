0bfc6f2ead6168c4fa7d51c3d444a65e9dffc333
adnansufian <a.sufian@uq.edu.au>
2020-08-19 13:10:26 +1000

    Removed single test suite. Constant is covered in existing unit tests. Resting is the same as the suzuki test.

--- test/granular/single/constant_velocity/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Clean directory
-./clean.sh
-
-#Run simulation
-cplexec -c 1 "./CFD_dummy_constant.py" -m 1 "lmp_cpl < lammps/constant.in" 
--- test/granular/single/terminal_velocity/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Clean directory
-./clean.sh
-
-#Run simulation
-cplexec -c 1 "./CFD_dummy_terminal.py" -m 1 "lmp_cpl < lammps/terminal.in" 
