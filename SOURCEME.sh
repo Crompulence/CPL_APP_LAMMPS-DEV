ff78efe81faa8f0cda571d99c6098bf353a54492
Gavin Pringle <gavin@epcc.ed.ac.uk>
2023-03-29 17:04:14 +0100

    Update SOURCEME.sh
    These changes means the user does not have to change directory to the SOURCEME.sh location to source it.  In other words, the user can simply run "source /path/to/CPL_APP_LAMMPS-DEV/SOURCEME.sh" from any directory.  This change does not break any workflow nor does it contradict existing READMEs, but it does extend the usability and allow for less fiddly batch scripts.

--- SOURCEME.sh
+++ SOURCEME.sh
@@ -1,7 +1,8 @@
-export PATH=$(pwd)/bin:$PATH
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export PATH=$CWD/bin:$PATH
 
-if [ -f CODE_INST_DIR ]; then
-	export LAMMPS_PATH=$(cat CODE_INST_DIR)
+if [ -f $CWD/CODE_INST_DIR ]; then
+        export LAMMPS_PATH=$(cat $CWD/CODE_INST_DIR)
 else
 	printf "ERROR:\n\tFile CODE_INST_DIR not found. Please provide one as described in the README.md file.\n"
 	return 1
