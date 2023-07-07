6aa56330002093ae136fc05c110ad6bcd788b45b
Gavin Pringle <gavin@epcc.ed.ac.uk>
2023-07-07 11:55:07 +0100

    Update example_archer2.bat
    corrected archer2 batch script

--- test/Couette_coupled/Partial_overlap/example_archer2.bat
+++ test/Couette_coupled/Partial_overlap/example_archer2.bat
@@ -4,7 +4,7 @@
 #SBATCH --time=0:10:0
 #SBATCH --exclusive
 #SBATCH --export=none
-#SBATCH --account=ecseaf01
+#SBATCH --account=y23
 
 #SBATCH --partition=standard
 #SBATCH --qos=standard
@@ -17,12 +17,11 @@ export OMP_NUM_THREADS=1
 module load other-software
 module load cpl-lammps
 
-# using your own installation: remove the last three lines and use these four lines instead
-# remember to update the path to the two SOURCEME.sh files
+# using your own installation: remove the last three lines and use these three 'module' lines and two 'source' lines instead remembering to update the path to your two SOURCEME.sh files
 #module load openfoam/com/v2106
 #module load cray-fftw
 #module load cray-python
-#source /work/y23/shared/cpl-openfoam-lammps/cpl-library/SOURCEME.sh
-#source /work/y23/shared/cpl-openfoam-lammps/CPL_APP_OPENFOAM/SOURCEME.sh
+#source /work/y23/y23/gavincpl/cpl-library/SOURCEME.sh
+#source /work/y23/y23/gavincpl/CPL_APP_OPENFOAM/SOURCEME.sh
 
 srun ${SHARED_ARGS} --het-group=0 --nodes=1 --tasks-per-node=1 lmp_cpl -in ./one_wall_imaginary_sliding_coupled.in :  --het-group=1 --nodes=1 --tasks-per-node=1 python ./python_dummy/CFD_test_vs_Couette.py
