a1738bc17c39dfc414ef4fd5de9d722f06f79905
Gavin Pringle <gavin@epcc.ed.ac.uk>
2023-07-07 14:10:04 +0100

    Update example_archer2.bat
    replaced shared module directory with own y23/gavincpl directory as the example to for user to employ their own installation as that looks like their setup.

--- test/Couette_coupled/Partial_overlap/example_archer2.bat
+++ test/Couette_coupled/Partial_overlap/example_archer2.bat
@@ -22,7 +22,7 @@ module load cpl-lammps
 #module load openfoam/com/v2106
 #module load cray-fftw
 #module load cray-python
-#source /work/y23/shared/cpl-openfoam-lammps/cpl-library/SOURCEME.sh
-#source /work/y23/shared/cpl-openfoam-lammps/CPL_APP_OPENFOAM/SOURCEME.sh
+#source /work/y23/y23/gavincpl/cpl-library/SOURCEME.sh
+#source /work/y23/y23/gavincpl/CPL_APP_OPENFOAM/SOURCEME.sh
 
 srun ${SHARED_ARGS} --het-group=0 --nodes=1 --tasks-per-node=1 lmp_cpl -in ./one_wall_imaginary_sliding_coupled.in :  --het-group=1 --nodes=1 --tasks-per-node=1 python ./python_dummy/CFD_test_vs_Couette.py
